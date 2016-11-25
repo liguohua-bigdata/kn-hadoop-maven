457adc4a9543ebc24355ae10bddac8b53c08226f
liguohua <2818461312@qq.com>
2016-11-25 19:37:03 +0800

    commit course

--- src/main/java/pa3/ParseWikiAndProfession.java
+++ src/main/java/pa3/ParseWikiAndProfession.java
@@ -18,6 +18,7 @@ import utils.StringDevUitl;
 import java.io.IOException;
 
 public class ParseWikiAndProfession {
+    //处理成name-->other的形式
     private static void parseFileLine(Mapper<LongWritable, Text, Text, Text>.Context context, String[] pairs) throws IOException, InterruptedException {
         if (pairs.length == 2) {
             String name = pairs[0].trim();
@@ -28,13 +29,18 @@ public class ParseWikiAndProfession {
         }
     }
 
+    /**
+     * 用于处理profesion.txt用 ':'切分文件中的每一行
+     */
     static class MyMapperProfession extends Mapper<Text, Text, Text, Text> {
         protected void map(Text key, Text value, org.apache.hadoop.mapreduce.Mapper<LongWritable, Text, Text, Text>.Context context) throws java.io.IOException, InterruptedException {
             String[] pairs = value.toString().trim().split(":");
             parseFileLine(context, pairs);
         }
     }
-
+    /**
+     * 用于处理wiki用 '\t'切分文件中的每一行
+     */
     static class MyMapperWiki extends Mapper<Text, Text, Text, Text> {
         protected void map(Text key, Text value, org.apache.hadoop.mapreduce.Mapper<LongWritable, Text, Text, Text>.Context context) throws java.io.IOException, InterruptedException {
             String[] pairs = value.toString().trim().split("\t");
@@ -42,7 +48,9 @@ public class ParseWikiAndProfession {
         }
     }
 
-
+    /**
+     * 用于两个mapper的reduce操作，将name作为文件名，写出到多个文件中。
+     */
     static class MyReduceProfessionAndWiki extends Reducer<Text, Text, Text, Text> {
         private MultipleOutputs multipleOutputs;
 
@@ -66,32 +74,36 @@ public class ParseWikiAndProfession {
     }
 
     public static void main(String[] args) throws Exception {
-
+        //##################可被删除的代码###############################
         args = new String[3];
         args[0] = "hdfs://qingcheng11:9000/input/mahout/pa3/pre/p";
         args[1] = "hdfs://qingcheng11:9000/input/mahout/pa3/pre/w";
         args[2] = "hdfs://qingcheng11:9000/input/mahout/pa3/raw/";
-        //1. check parameters
+        HdfsDevUtil.deleteFileOnExist(args[2]);
+        //##################可被删除的代码###############################
+
+        //1. 检测出肉参数
         if (args.length != 3) {
             System.err.println("Usage: GetProfessionLemma  <path to professions.txt>  <path to wiki-big-lemma-index>  <output-dir> ");
             System.exit(-1);
         }
-        HdfsDevUtil.deleteFileOnExist(args[2]);
-        // 创建job
+
+        //2. 创建job
         final Job job = new Job(new Configuration(), "pa3");
         job.setJarByClass(ParseWikiAndProfession.class);
-        //设置mapper输出格式
+        //3.设置mapper输出格式
         job.setMapOutputKeyClass(Text.class);
         job.setMapOutputValueClass(Text.class);
 
-        // 设置reduce
+        //4.设置reduce
         job.setReducerClass(MyReduceProfessionAndWiki.class);
         job.setOutputKeyClass(Text.class);
         job.setOutputValueClass(Text.class);
-        // 设置最终结果输出路径
+        //5.设置最终结果输出路径
         MultipleInputs.addInputPath(job, new Path(args[0]), TextInputFormat.class, MyMapperProfession.class);
         MultipleInputs.addInputPath(job, new Path(args[1]), TextInputFormat.class, MyMapperWiki.class);
         FileOutputFormat.setOutputPath(job, new Path(args[2]));
+        //6.提交job
         job.waitForCompletion(true);
     }
 }  
